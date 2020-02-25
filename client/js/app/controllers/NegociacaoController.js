bb1e1deb433a39530d537336b0855e145eed1966
thayron matos <thayronf3@gmail.com>
2020-02-25 16:03:41 -0300

    utilizando o $ (cifrão) para atribuir o document.querySelector.bind(document) fazendo assim, podemos utilizar o $ (cifrão) para procurar uma div ou uma classe utilizando o celetor

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,20 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value)
+        console.log(this.inputValor.value);
+
+    }
+
+}
