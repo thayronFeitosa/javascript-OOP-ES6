9d8352717aadf97ecf90613ef6c8f3994f172567
thayron matos <thayronf3@gmail.com>
2020-02-25 21:34:52 -0300

    Foi ultilizado a função map do javascript e utilizado o epread operador, para indicar que o array será desmenbrado e o primeiro item do array, e cada parametro do Date será posicionado na mesma ordem no contrutor.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,20 +1,24 @@
 class NegociacaoController {
 
     constructor() {
-        let $ = document.querySelector.bind(document);
+            let $ = document.querySelector.bind(document);
 
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
-    }
-
+            this._inputData = $('#data');
+            this._inputQuantidade = $('#quantidade');
+            this._inputValor = $('#valor');
+        }
+        // utilizando a função map e utilizado o spread operador indeicando que o array será desmenbrado
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.replace(/-/g, ','));
-        console.log(data);
-        console.log(this._inputQuantidade.value)
-        console.log(this._inputValor.value);
+        let dat = new Date(...this._inputQuantidade
+            .split('-')
+            .map(function(item, indice) {
+                return item - indice % 2;
+
+            })
+
+        );
 
     }
 
