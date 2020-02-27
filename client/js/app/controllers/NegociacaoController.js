73ec79e26f7a94efc15a89ac53846d5022fc9178
thayron matos <thayronf3@gmail.com>
2020-02-27 04:58:49 -0300

    algumas alterações necessárias na class NegociacaoController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,15 +13,26 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let negociacao =
-            new Negociacao(
-                DateHelper.textoParaData(this._inputData.value),
-                this._inputQuantidade.value,
-                this._inputValor.value
-            );
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        this._listaNegociacoes.adiciona(negociacao);
         console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+        this._inputData.focus();
+
     }
 
 
